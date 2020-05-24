a49570f10d82434b41bd3f259dbb12441e115682
Jason Bryant <jasonbryant@invisionapp.com>
2020-05-24 17:14:14 +0100

    viewport and readme update

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ export default class CustomDocument extends Document {
             <script type="text/javascript" async="" src="http://localhost:3000/assets/js/geolocation.js"></script>
             <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Lora:400,700|Raleway:300,400,500|Mrs+Saint+Delafield|Oswald:400,500,600,700|Montserrat:400,500,600,700,800,900" rel="stylesheet"
             />
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
             <meta name="google" value="notranslate" />
         </Head>
 
