eaf7dd29c8320c03f8f5653c6c06ad4587917f4c
Jason Bryant <jasonbryant@invisionapp.com>
2020-07-06 06:38:30 +0100

    fetch and ip api

--- server/utils/segment.helper.js
+++ server/utils/segment.helper.js
@@ -3,40 +3,61 @@ const Analytics = require('analytics-node'),
     analytics = new Analytics(process.env.SEGMENT_KEY),
     environment = process.env.ENVIRONMENT,
     crypto = require('crypto'),
-    shasum = crypto.createHash('sha1');
+    geo = require('./geo')
 
 module.exports = {
-    pageCall: (url_parts) => {
+    pageCall: async (url_parts) => {
         const pathname = url_parts.pathname
         if (pathname !== '/' && pathname !== '/about' && pathname !== '/rebuild')
             return
+       
+        const noGeoResult = {
+            city: 'unknown',
+            country: 'unknown'
+        }
 
-        const info = map[pathname],
-            userId = crypto.createHash('md5').update(url_parts.ipaddress).digest('hex')
-        
-        analytics.page({
-            userId: userId,
-            category: info.category,
-            name: info.name,
-            properties: {
-                url: 'http://jasonreidbryant.com',
-                environment: environment,
-                path: url_parts.pathname,
-                title: info.title,
-                referer: url_parts.referer,
-                eventAction: 'Page View',
-                eventCategory: info.category,
-                eventLabel: info.title,
-            },
-            context: {
-                ip: url_parts.ipaddress,
-                userAgent: url_parts.user_agent
-              },
-        })
+        try {
+            // let geoPromiseObject = await geo.getLocation(url_parts.ipaddress)
+            const promise = await geo.getLocation(url_parts.ipaddress),
+                geoResult = await Promise.resolve(promise), //all
+                result = geoResult.status === 'success' ? geoResult : noGeoResult
+                console.log('result', result)
+                
+                pageCall(url_parts, result)
+        } catch(e) {
+            console.log('geoInfo Error!', e)
+            pageCall(url_parts, noGeoResult)
+        }
     }
 }
+const pageCall = (url_parts, geoResult) => {
+    const info = map[url_parts.pathname],
+        userId = crypto.createHash('md5').update(url_parts.ipaddress).digest('hex')
 
-const map = {
+    analytics.page({
+        userId: userId,
+        category: info.category,
+        name: info.name,
+        properties: {
+            url: 'http://jasonreidbryant.com',
+            ipaddress: url_parts.ipaddress,
+            city: geoResult.city,
+            country: geoResult.country,
+            environment: environment,
+            path: url_parts.pathname,
+            title: info.title,
+            referer: url_parts.referer,
+            eventAction: 'Page View',
+            eventCategory: info.category,
+            eventLabel: info.title,
+        },
+        context: {
+            ip: url_parts.ipaddress,
+            userAgent: url_parts.user_agent
+            },
+    })
+},
+map = {
     '/': {
         title: 'Home',
         name: 'Home',
